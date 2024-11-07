fa8cb7255608cf6d5e86b7d568e390be1b16dcf9
PebPeb <67885698+PebPeb@users.noreply.github.com>
2024-11-06 22:26:00 -0500

    Verilator

--- build-utils/build.mk
+++ build-utils/build.mk
@@ -6,7 +6,43 @@ $(foreach x, $(TB_SOURCE), \
 	$(eval $(call gen_build_next, $(x), $$(BUILD_DIR_$(BUILD_NAME)))) \
 )
 endif
+
+ifneq ($(V_COMPILE),)
+$(foreach x, $(V_COMPILE), \
+	$(eval $(call verilator_build_stage, $(x), $$(BUILD_DIR_$(BUILD_NAME)))) \
+)
+
+
+
+
+.PHONY: Test_$(BUILD_NAME)
+Test_$(BUILD_NAME): 
+	@echo $(V_COMPILE)
+endif
+
 endef
+# $(eval $(call verilator_build, $$(BUILD_DIR_$(BUILD_NAME))))
+
+# define verilator_build
+
+# .PHONY: compile-v-$(BUILD_NAME)
+# compile-v-$(BUILD_NAME): $(wildcard V_STAGE_$(BUILD_NAME)*)
+# 	@echo Test
+
+# endef
+
+define verilator_build_stage
+STAGE := $(1)
+ifneq ($(1),)
+.PHONY: V_STAGE_$(BUILD_NAME)_$(STAGE)
+V_STAGE_$(BUILD_NAME)_$(STAGE):
+	@echo V_STAGE_$(BUILD_NAME)_$(STAGE)
+	@echo V_SOURCE_$(STAGE)
+	cd $(2) && \
+	verilator -Wall --cc $$(V_SOURCE_$(STAGE)) --top-module $(STAGE) --trace --Wno-DECLFILENAME --Wno-PINCONNECTEMPTY -Wno-UNUSED
+endif
+endef
+
 
 define gen_build_next
 ifeq ($(suffix $(1)),.v)
@@ -16,11 +52,12 @@ build-iv-$(BUILD_NAME):
 	iverilog -o $(basename $(notdir $(1))).out -DVCD_DUMP=1 $(1) $(TB_INCLUDE) && \
 	vvp $(basename $(notdir $(1))).out
 endif
-
-ifeq ($(suffix $(1)),.cpp)
-.PHONY: build-v-$(BUILD_NAME)
-build-v-$(BUILD_NAME):
-	cd $(2) && \
-	verilator -Wall --cc $(TB_INCLUDE) --top-module $(TB_TOP_MODULE) --trace --Wno-DECLFILENAME --Wno-PINCONNECTEMPTY -Wno-UNUSED
-endif
 endef
+
+
+# ifeq ($(suffix $(1)),.cpp)
+# .PHONY: build-v-$(BUILD_NAME)
+# build-v-$(BUILD_NAME):
+# 	cd $(2) && \
+# 	verilator -Wall --cc $(TB_INCLUDE) --top-module $(TB_TOP_MODULE) --trace --Wno-DECLFILENAME --Wno-PINCONNECTEMPTY -Wno-UNUSED
+# endif
